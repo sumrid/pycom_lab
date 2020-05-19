318fb58276d2a03996f3873203771f233a33bd3d
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2020-05-19 15:12:21 +0700

    [update] lorawan OTAA

--- LoRaWAN_OTAA/main.py
+++ LoRaWAN_OTAA/main.py
@@ -1,41 +1,49 @@
 from network import LoRa
 import socket
 import time
-import ubinascii # micropython module
-import struct # for ABP
+import ubinascii  # micropython module
+import struct  # for ABP
+import pycom
+pycom.heartbeat(False)
+pycom.rgbled(0xECEB1A)
 
-# asia region
-lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.AS923, sf=12)
+# create LoRa asia region
+lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.AS923, device_class=LoRa.CLASS_C)
 print('Device EUI:', ubinascii.hexlify(lora.mac()).upper().decode('utf-8'))
 
 
-##########
-#  OTAA
-##########
-app_eui = ubinascii.unhexlify('70B3D57ED002F1F4')
-app_key = ubinascii.unhexlify('D569F48B31D7D92D702662A34C6875B5')
+################
+#     OTAA
+################
+app_eui = ubinascii.unhexlify('D8DF149D1DA65EF9')
+app_key = ubinascii.unhexlify('E8F1B92F79A72A8C2AA8AE182ADF2B35')
 lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 while not lora.has_joined():
     time.sleep(2.5)
     print('Not yet joined...')
+pycom.rgbled(0x27ED00)
+print('Joined!')
 
-##########
-#  ABP
-##########
-# dev_addr = struct.unpack(">l", ubinascii.unhexlify('260414D6'))[0]
-# nwk_swkey = ubinascii.unhexlify('7E457BB450EC7646C1DCB186832708E4')
-# app_swkey = ubinascii.unhexlify('747D1E0CD854092C40EB4B55670E49DD')
-# lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
-
-print("Join success.", lora.has_joined())
-
-
+# create a LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 s.setblocking(False)
-s.send(bytes([0x01, 0x02, 0x03]))
-s.settimeout(5.0)
 
-# get any data received (if any...)
-data = s.recv(64)
-print('received: ', data)
+# uplink
+s.send("Hello from Pycom4")
+
+# วนลูปรอรับข้อมูล
+while True:
+    time.sleep(3)
+
+    data = s.recv(64)    # recive downlink data
+    print("Data:", data) # <-- bytes
+
+    if data != b'':
+        try:
+            hex_val = ubinascii.hexlify(data)
+            number = int(hex_val, 16)
+            pycom.rgbled(number)          # set RGB LED
+        except:
+            pycom.rgbled(0xFF0000)
+
--- MQTT/README.md
+++ MQTT/README.md
@@ -1,2 +1,6 @@
+## MQTT
+
+* [[How to] Get started subscribing and publishing messages, in MicroPython, using MQTT!](https://pycom.io/get-started-subscribing-and-publishing-messages-in-micropython-using-mqtt/)
+* [Simple MQTT Tutorial](https://forum.pycom.io/topic/211/simple-mqtt-tutorial/3)
 * [ WiPy & Publishing/Subscribing With MQTT](https://www.youtube.com/watch?v=3NDPSbr5J14&index=3&list=PLS09TA19umCOavPeP_FoMHoCuCzjJUWQk&t=7s)
 * [MQTT example - pycom](https://docs.pycom.io/tutorials/all/mqtt/)
